2b4dacbe56ca331309424692061400aca124f291
Felipe Rios <felipedeoliveirarios@gmail.com>
2018-08-16 17:07:26 -0300

    Tentativa de consertar um erro na função de encaminhamento do suporte.

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def resposta_do_suporte(bot, update):
     # Encaminha uma resposta vinda do grupo de suporte de volta para o usuário.
     if update.message.reply_to_message and update.message.reply_to_message.forward_from:
         bot.send_message(chat_id=int(update.message.reply_to_message.forward_from.id), text="O suporte enviou uma resposta:")
-        bot.forward_message(chat_id=imessagnt(update.message.reply_to_message.forward_from.id), from_chat_id=update.message.chat_id, message_id=update.message.message_id)
+        bot.forward_message(chat_id=update.message.reply_to_message.forward_from.id, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
         return ConversationHandler.END
 
 """-------------------------------------------------------------------------"""
