12690cc1fd035d1d9f161815cdabeb58cc10fe29
Felipe Rios <felipedeoliveirarios@gmail.com>
2018-08-16 17:12:33 -0300

    Consertado o problema com a função de resposta do suporte.

--- main.py
+++ main.py
@@ -51,12 +51,11 @@ def contato_com_suporte(bot, update):
     
 """-------------------------------------------------------------------------"""
 
+# Encaminha uma resposta vinda do grupo de suporte de volta para o usuário.
 def resposta_do_suporte(bot, update):
-    # Encaminha uma resposta vinda do grupo de suporte de volta para o usuário.
-    if update.message.reply_to_message and update.message.reply_to_message.forward_from:
-        bot.send_message(chat_id=int(update.message.reply_to_message.forward_from.id), text="O suporte enviou uma resposta:")
-        bot.forward_message(chat_id=update.message.reply_to_message.forward_from.id, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
-        return ConversationHandler.END
+  bot.send_message(chat_id=int(update.message.reply_to_message.forward_from.id), text="O suporte enviou uma resposta:")
+  bot.forward_message(chat_id=update.message.reply_to_message.forward_from.id, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
+  return ConversationHandler.END
 
 """-------------------------------------------------------------------------"""
 
