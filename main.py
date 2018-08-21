403da137fb9c74136d192da656efa5eea19012aa
Felipe Rios <felipedeoliveirarios@gmail.com>
2018-08-21 15:35:49 -0300

    Refinamento da função que lida com o gerenciamento de fichas.

--- main.py
+++ main.py
@@ -57,72 +57,70 @@ def get_chat_id(bot, update):
 def desconhecido(bot, update):
     chat_id = get_chat_id(bot, update)
     msg = "Desculpe, não consigo entender esse comando."
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    bot.send_message(chat_id=chat_id, text=msg)
     msg = "Utilize o comando /ajuda para exibir a lista de comandos."
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    bot.send_message(chat_id=chat_id, text=msg)
 
 """-------------------------------------------------------------------------"""
 
 # Inicia o tratamento de uma requisição de suporte.
 def suporte(bot, update):
+    chat_id = get_chat_id(bot, update)
     msg = "Qual é o problema?\n(Descreva da forma mais clara que puder)"
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    bot.send_message(chat_id=chat_id, text=msg)
     msg = "(Utilize o comando /cancelar para cancelar o pedido de suporte)"
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    bot.send_message(chat_id=chat_id, text=msg)
     return SUPORTE_PERGUNTA_DUVIDA
 
 """-------------------------------------------------------------------------"""
 
 # Encaminha uma mensagem do usuário para o grupo de suporte.
 def contato_com_suporte(bot, update):
-<<<<<<< HEAD
-    bot.forward_message(chat_id=support_chat_id, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
-    
-=======
+    chat_id = get_chat_id(bot, update)
     # Encaminha uma mensagem do usuário para o grupo de suporte.
-    bot.forward_message(chat_id=support_chat_id, from_chat_id=update.message.chat_id, message_id=update.message.message_id)            
->>>>>>> f6f70434579a3f33b30af80d3993d64fd30a91c3
+    bot.forward_message(chat_id=support_chat_id, from_chat_id=chat_id, message_id=update.message.message_id)
     msg = "Encaminhei sua mensagem para o suporte. Te aviso assim que tiver uma resposta."
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    bot.send_message(chat_id=chat_id, text=msg)
     msg = "Desde já, peço desculpas pelo transtorno"
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
+    bot.send_message(chat_id=chat_id, text=msg)
     return SUPORTE_ENCAMINHA_RESPOSTA
     
 """-------------------------------------------------------------------------"""
 
 # Encaminha uma resposta vinda do grupo de suporte de volta para o usuário.
 def resposta_do_suporte(bot, update):
-<<<<<<< HEAD
-    if update.message.reply_to_message and update.message.reply_to_message.forward_from:
+    chat_id = get_chat_id(bot, update)
+    logging.debug("Entrada na função \"resposta_do_suporte\"")
+    if update.message.reply_to_message and update.message.reply_to_message.forward_from and chat_id == support_chat_id:
+        logging.debug("O update é uma resposta do suporte.")
         msg = "O suporte enviou uma resposta:"
         bot.send_message(chat_id=update.message.reply_to_message.forward_from.id, text=msg)
         bot.forward_message(chat_id=update.message.reply_to_message.forward_from.id, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
         return ConversationHandler.END
-=======
-  logging.debug("Entrada na função \"resposta_do_suporte\"")
-  if update.message.reply_to_message and update.message.chat.id == support_chat_id:
-    logging.debug("O update é uma resposta do suporte.")
-    bot.send_message(chat_id=update.message.reply_to_message.forward_from.id, text="O suporte enviou uma resposta:")
-    bot.forward_message(chat_id=update.message.reply_to_message.forward_from.id, from_chat_id=update.message.chat_id, message_id=update.message.message_id)
-    return ConversationHandler.END
-  else:
-    logging.debug("O update não é uma resposta do suporte.")
-    return SUPORTE_ENCAMINHA_RESPOSTA
->>>>>>> f6f70434579a3f33b30af80d3993d64fd30a91c3
+    else:
+      logging.debug("O update não é uma resposta do suporte.")
+      return SUPORTE_ENCAMINHA_RESPOSTA
 
 """-------------------------------------------------------------------------"""
 
 # Finaliza a execução de um ConversationHandler
 def cancelar(bot, update):
+    chat_id = get_chat_id(bot, update)
     msg = "Tudo bem. Estarei aqui se precisar de mim."
-    bot.send_message(chat_id=update.message.chat_id, text=msg)
-<<<<<<< HEAD
-=======
+    bot.send_message(chat_id=chat_id, text=msg)
+    return ConversationHandler.END
+
+"""-------------------------------------------------------------------------"""
+
+# Finaliza a execução do ConversationHandler de suporte
+def cancelar_suporte(bot, update):
+    chat_id = get_chat_id(bot, update)
+    msg = "Tudo bem. Estarei aqui se precisar de mim."
+    bot.send_message(chat_id=chat_id, text=msg)
     msg = "O usuário @{} ({} {}) cancelou uma requisição de suporte".format(update.message.from_user.username,
                                                                            update.message.from_user.first_name,
                                                                            update.message.from_user.last_name)
-    bot.send_message(chat_id=int(support_chat_id), text=msg)
->>>>>>> f6f70434579a3f33b30af80d3993d64fd30a91c3
+    bot.send_message(chat_id=support_chat_id, text=msg)
     return ConversationHandler.END
 
 """-------------------------------------------------------------------------"""
@@ -137,7 +135,7 @@ def setup(bot, update):
             bot.send_message(chat_id=update.message.chat_id, text=msg)
             msg = "Não se preocupe. A não ser que esse grupo seja recém-criado, não é um problema."
             bot.send_message(chat_id=update.message.chat_id, text=msg)
-            msg = "Caso esse grupo seja recém-criado, use o comando /suporte, e resolveremos seu problema em breve."
+            msg = "Caso esse grupo seja recém-criado, use o comando /suporte, diga que recebeu essa mensagem, e resolveremos seu problema rapidinho."
             bot.send_message(chat_id=update.message.chat_id, text=msg)
             return ConversationHandler.END
         
@@ -211,18 +209,29 @@ def start(bot, update):
 
 """-------------------------------------------------------------------------"""
 
+# Função que trata da manipulação de fichas
 def ficha(bot, update):
-    # Caso já exista uma entrada da combinação de usuário e grupo no BD...
-    if database.confere_usuario(update.message.chat_id, update.message.from_user.id):
-        # Recupera e envia as informações básicas do personagem.
-        pers = database.get_informação_basica(update.message.chat_id, update.message.from_user.id)
-        msg = "{}, Nv. {}\n{} {}, {}".format(pers.nome, pers.nivel, pers.ident, pers.sexo, pers.idade)
-        bot.send_message(chat_id=update.message.chat_id, text=msg, reply_markup = inline_kb_markup)
+    chat_id = get_chat_id(bot, update)
+    user_id = get_user_id(bot, update)
+    # Caso já exista uma entrada da combinação de usuário e grupo no BD ou
+    # o usuário seja um adminstrador acessando os dados...
+    if database.confere_usuario(chat_id, user_id) or is_group_admin(bot, user_id, chat_id):
+        # Caso o usuário esteja num chat privado ou grupo onde não é administrador
+        if chat_id == user_id or not is_group_admin(bot, user_id, chat_id):
+            # Recupera e envia as informações básicas do personagem.
+            pers = database.get_informação_basica(chat_id, user_id)
+            msg = "{}, Nv. {}\n{} {}, {}\nPossui {} pontos não-utilizados.".format(pers.nome,
+                                                                                   pers.nivel,
+                                                                                   pers.ident,
+                                                                                   pers.sexo,
+                                                                                   pers.idade,
+                                                                                   pers.pontos)
+            bot.send_message(chat_id=chat_id, text=msg)
         
         msg = "E então, o que tem em mente?"
         
-        keyboard = [[telegram.InlineKeyboardButton("Editar Ficha", callback_data = "Editar")],
-                    [telegram.InlineKeyboardButton("Excluir Ficha", callback_data = "Excluir")],
+        keyboard = [[telegram.InlineKeyboardButton("Editar Ficha", callback_data = "Editar Ficha")],
+                    [telegram.InlineKeyboardButton("Excluir Ficha", callback_data = "Excluir Ficha")],
                     [telegram.InlineKeyboardButton("Importar/Exportar Ficha", callback_data = "Imp/Exp")],
                     [telegram.InlineKeyboardButton("Sair", callback_data = "Sair")]]
         inline_kb_markup = telegram.InlineKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
@@ -245,45 +254,93 @@ def ficha(bot, update):
         # Passa para o próximo estado, que irá conduzir a outro
         # ConversationHandler para acompanhar o usuário na criação de ficha.
         return FICHA_CRIAR
-
+  
 """-------------------------------------------------------------------------"""
 
 # Trata a resposta enviada pelo menu exibido em /ficha quando o usuário
 # já possúi uma ficha.
 def ficha_menu(bot, update):
     query = update.callback_query
+    chat_id = get_chat_id(bot, update)
+    user_id = get_user_id(bot, update)
     # Remove o teclado após receber a resposta.
     rmv_inline_keyboard(bot, query)
+    
+    # Caso o usuário seja administrador do grupo onde está acessando as fichas...
+    if chat_id != user_id and is_group_admin(bot, user_id, chat_id):
+        # cria um array de strings de nomes de usuário, tendo como índice a id de usuário. 
+        id_fichas = database.get_id_fichas(id_grupo)
+        chat_users = []
+        for id_ficha in id_fichas:
+            try:
+                chat_member = get_chat_member(id_grupo, id_ficha)
+                chat_users[id_ficha] = '@' + chat_member.user.username
+            except:
+                debug("Entrada do usuário {} no grupo {} está abandonada.".format(id_ficha, id_grupo))
+                
+        # Trata a resposta do usuário via menu.
+        if query.data == "Editar":
+            # Cria um teclado para selecionar o usuário cuja ficha será editada.
+            
+            # Inicia o ConversationHandler de edição de fichas
+            ficha_editar(bot, update)
+            return ficha(bot, update)
 
-    # Trata a resposta do usuário via menu.
-    if query.data == "Editar":
-        # Inicia o ConversationHandler de edição de fichas
-        ficha_editar(bot, update)
-        return FICHA_RESET
-
-    elif query.data == "Excluir":
-        msg = "Uau. Essa é uma decisão bem séria. Tem certeza?"
-        keyboard = [[telegram.InlineKeyboardButton("Sim", callback_data = "Sim")],
+        elif query.data == "Excluir":
+            msg = "Uau. Essa é uma decisão bem séria. Tem certeza?"
+            keyboard = [[telegram.InlineKeyboardButton("Sim", callback_data = "Sim")],
                     [telegram.InlineKeyboardButton("Não", callback_data = "Não")]]
-        inline_kb_markup = telegram.InlineKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
-        bot.send_message(chat_id=query.message.chat_id, text=msg, reply_markup = inline_kb_markup)
-        return FICHA_EXCLUIR
+            inline_kb_markup = telegram.InlineKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
+            bot.send_message(chat_id=chat_id, text=msg, reply_markup = inline_kb_markup)
+            return FICHA_EXCLUIR
 
-    elif query.data == "Imp/Exp":
-        msg = "Importar ou exportar?"
-        keyboard = [[telegram.InlineKeyboardButton("Importar", callback_data = "IMP")],
+        elif query.data == "Imp/Exp":
+            msg = "Importar ou exportar?"
+            keyboard = [[telegram.InlineKeyboardButton("Importar", callback_data = "IMP")],
                     [telegram.InlineKeyboardButton("Exportar", callback_data = "EXP")]]
-        inline_kb_markup = telegram.InlineKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
-        bot.send_message(chat_id=query.message.chat_id, text=msg, reply_markup = inline_kb_markup)
-        return FICHA_IMP/EXP
+            inline_kb_markup = telegram.InlineKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
+            bot.send_message(chat_id=chat_id, text=msg, reply_markup = inline_kb_markup)
+            return FICHA_IMP/EXP
         
-    elif query.data == "Sair":
-        msg = "Ok! Me chame de novo se precisar de algo."
-        bot.send_message(chat_id=query.message.chat_id, text=msg)
-        return ConversationHandler.END
+        elif query.data == "Sair":
+            msg = "Ok! Me chame de novo se precisar de algo."
+            bot.send_message(chat_id=chat_id, text=msg)
+            return ConversationHandler.END
 
+        else:
+            return FICHA_MENU
+    
+    # Caso o usuário não seja administrador do grupo...
     else:
-        return FICHA_MENU
+        # Trata a resposta do usuário via menu.
+        if query.data == "Editar":
+            # Inicia o ConversationHandler de edição de fichas
+            ficha_editar(bot, update)
+            return ficha(bot, update)
+
+        elif query.data == "Excluir":
+            msg = "Uau. Essa é uma decisão bem séria. Tem certeza?"
+            keyboard = [[telegram.InlineKeyboardButton("Sim", callback_data = "Sim")],
+                    [telegram.InlineKeyboardButton("Não", callback_data = "Não")]]
+            inline_kb_markup = telegram.InlineKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
+            bot.send_message(chat_id=chat_id, text=msg, reply_markup = inline_kb_markup)
+            return FICHA_EXCLUIR
+
+        elif query.data == "Imp/Exp":
+            msg = "Importar ou exportar?"
+            keyboard = [[telegram.InlineKeyboardButton("Importar", callback_data = "IMP")],
+                    [telegram.InlineKeyboardButton("Exportar", callback_data = "EXP")]]
+            inline_kb_markup = telegram.InlineKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
+            bot.send_message(chat_id=chat_id, text=msg, reply_markup = inline_kb_markup)
+            return FICHA_IMP/EXP
+        
+        elif query.data == "Sair":
+            msg = "Ok! Me chame de novo se precisar de algo."
+            bot.send_message(chat_id=chat_id, text=msg)
+            return ConversationHandler.END
+
+        else:
+            return FICHA_MENU
 
 """-------------------------------------------------------------------------"""
 
@@ -298,7 +355,7 @@ def ficha_criar(bot, update):
         # Caso o usuário decida por criar uma ficha, se iniciará um
         # ConversationHandler através da função para guiá-lo no processo
         ficha_criar_nome(bot, update)
-        return FICHA_RESET
+        return ficha(bot, update)
     else:
         # Caso o usuário não tenha uma ficha nem possua interesse em
         # criar uma no momento, não há nada a se fazer além de finalizar
@@ -316,10 +373,10 @@ def ficha_excluir(bot, update):
         database.del_ficha(query.message.chat_id, query.from_user.id)
         msg = "Tá legal. Está feito."
         bot.send_message(chat_id=query.message.chat_id, text=msg)
-        return FICHA_RESET
+        return ficha(bot, update)
     else:
         # Caso o usuário decida não excluir a ficha...
-        return FICHA_RESET
+        return ficha(bot, update)
 
 """-------------------------------------------------------------------------"""
 
@@ -341,7 +398,7 @@ def ficha_impexp():
         bot.send_message(chat_id=query.message.chat_id, text=msg)
         return FICHA_EXP
     else:
-        return FICHA_RESET
+        return ficha(bot, update)
 
 """#########################################################################"""
 
@@ -369,7 +426,7 @@ support_handler = ConversationHandler(
         SUPORTE_ENCAMINHA_RESPOSTA: [RegexHandler('', resposta_do_suporte)],
         },
     
-    fallbacks=[CommandHandler('cancelar', cancelar)]
+    fallbacks=[CommandHandler('cancelar', cancelar_suporte)]
     )
 
 dispatcher.add_handler(support_handler)
@@ -398,7 +455,7 @@ dispatcher.add_handler(setup_handler)
 # Esse handler trata do menu de fichas e conduz às operações mais específicas
 # do CRUD, e de importação e exportação.
 
-FICHA_MENU, FICHA_CRIAR, FICHA_EXCLUIR, FICHA_IMP/EXP, FICHA_IMP, FICHA_EXP, FICHA_RESET = range(7)
+FICHA_MENU, FICHA_CRIAR, FICHA_EXCLUIR, FICHA_IMP/EXP, FICHA_IMP, FICHA_EXP = range(6)
 
 sheet_handler = ConversationHandler(
     entry_points = [CommandHandler('ficha', ficha)],
@@ -409,7 +466,6 @@ sheet_handler = ConversationHandler(
         FICHA_IMP/EXP: [CallbackQueryHandler(ficha_impexp)],
         FICHA_IMP: [CallbackQueryHandler()],
         FICHA_EXP: [CallbackQueryHandler()],
-        FICHA_RESET:[],
     },
     fallbacks = [CallbackQueryHandler()],
     per_chat = True
